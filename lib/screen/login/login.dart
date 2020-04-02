bdbfc54f4b7cefd3a6c9d8016e04f58b9cb653f8
cheeyon97 <cheeyon97@hotmail.com>
2020-04-02 17:39:26 +0800

    login and logout screen

---
+++ lib/screen/login/login.dart
@@ -0,0 +1,327 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+
+// import 'package:flutter_ui_splash_screen/homeScreen.dart';
+// import 'signup.dart';
+//import 'package:google_fonts/google_fonts.dart';
+
+import '../../Widget/bezierContainer.dart';
+import '../home_page/home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  final toggleBrightness;
+
+  LoginPage(
+      {Key key, this.title = 'User Settings', @required this.toggleBrightness})
+      : super(key: key);
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _validate = false;
+  final _text = TextEditingController();
+
+  @override
+  void dispose() {
+    _text.dispose();
+    super.dispose();
+  }
+
+
+
+  Widget _passwordEntryField(String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+              controller: _text,
+              style:
+                  new TextStyle(color: Colors.black, height: 1.0, fontSize: 18),
+              obscureText: true,
+              decoration: InputDecoration(
+                  errorText:
+                      _validate ? 'The password field cannot be empty' : null,
+                  border: InputBorder.none,
+                  fillColor: Color(0xfff3f3f4),
+                  filled: true,
+                  contentPadding: const EdgeInsets.all(10.0)))
+        ],
+      ),
+    );
+  }
+
+  Widget _usernameEntryField(String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+              style:
+                  new TextStyle(color: Colors.black, height: 1.0, fontSize: 18),
+              decoration: InputDecoration(
+                  errorText:
+                      _validate ? 'The username field cannot be empty' : null,
+                  border: InputBorder.none,
+                  fillColor: Color(0xfff3f3f4),
+                  filled: true,
+                  contentPadding: const EdgeInsets.all(10.0)))
+        ],
+      ),
+    );
+  }
+
+  Widget _submitButton() {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          _text.text.isEmpty ? _validate = true : _validate = false;
+        });
+        if (_validate == false){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage()));
+        }
+         
+      },
+      // onTap: () {
+      //    Navigator.push(
+      //        context, MaterialPageRoute(builder: (context) => LoginPage()));
+      // },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Color(0xfffbb448).withAlpha(100),
+                  offset: Offset(2, 4),
+                  blurRadius: 8,
+                  spreadRadius: 2)
+            ],
+            color: Color(0xfffbb448)),
+        child: Text(
+          'Login',
+          style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  Widget _createAccountLabel() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20),
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Don\'t have an account ?',
+            style: TextStyle(fontSize: 13, fontWeight: FontWeight.w600),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          InkWell(
+            onTap: () {
+              //  Navigator.push(context,
+              //      MaterialPageRoute(builder: (context) => SignUpPage()));
+            },
+            child: Text(
+              'Register',
+              style: TextStyle(
+                  color: Color(0xfff79c4f),
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _googleFacebookLogin() {
+    return Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            // color: Colors.blue,
+            height: 20,
+            width: 10,
+          ),
+          Expanded(
+            flex: 1,
+            child: FacebookSignInButton(
+              text: 'Facebook',
+              onPressed: null,
+              textStyle: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+          Container(
+            // color: Colors.blue,
+            height: 100,
+            width: 10,
+          ),
+          Expanded(
+            flex: 1,
+            child: GoogleSignInButton(
+              onPressed: null,
+              text: 'Google',
+              textStyle: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            
+            ),
+          ),
+          Container(
+            height: 20,
+            width: 10,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _title() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(children: [
+        TextSpan(
+          text: 'Budget',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+        TextSpan(
+          text: 'Go',
+          style: TextStyle(color: Color(0xffe46b10), fontSize: 30),
+        ),
+      ]),
+    );
+  }
+
+  Widget _usernamePasswordWidget() {
+    return Column(
+      children: <Widget>[
+        _usernameEntryField("Username"),
+        _passwordEntryField("Password"),
+      ],
+    );
+  }
+
+  Widget _divider() {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        children: <Widget>[
+          SizedBox(
+            width: 20,
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+              child: Divider(
+                thickness: 1,
+              ),
+            ),
+          ),
+          Text('or sign in with'),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+              child: Divider(
+                thickness: 1,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Container(
+      alignment: Alignment.center,
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 125,
+                ),
+                _title(),
+                SizedBox(
+                  height: 50,
+                ),
+                _usernamePasswordWidget(),
+                SizedBox(
+                  height: 20,
+                ),
+                _submitButton(),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  alignment: Alignment.centerRight,
+                  child: Text('Forgot Password ?',
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.w500)),
+                ),
+                _divider(),
+                _googleFacebookLogin(),
+                SizedBox(
+                  height: 5,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: SizedBox(),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: _createAccountLabel(),
+          ),
+           Positioned(
+               top: -MediaQuery.of(context).size.height * .15,
+               right: -MediaQuery.of(context).size.width * .4,
+               child: BezierContainer())
+        ],
+      ),
+    )));
+  }
+}
---
+++ lib/screen/signout/signout.dart
@@ -0,0 +1,139 @@
+import 'package:budgetgo/screen/login/login.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
+
+
+import '../../Widget/bezierContainer.dart';
+import '../home_page/home_page.dart';
+
+class LogoutPage extends StatefulWidget {
+  final toggleBrightness;
+
+  LogoutPage(
+      {Key key, this.title = 'User Settings', @required this.toggleBrightness})
+      : super(key: key);
+
+  final String title;
+
+  @override
+  _LogoutPageState createState() => _LogoutPageState();
+}
+
+class _LogoutPageState extends State<LogoutPage> {
+  
+  
+  
+  Widget _submitButton() {
+    return InkWell(
+      onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
+        
+         
+      },
+      // onTap: () {
+      //    Navigator.push(
+      //        context, MaterialPageRoute(builder: (context) => LoginPage()));
+      // },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Color(0xfffbb448).withAlpha(100),
+                  offset: Offset(2, 4),
+                  blurRadius: 8,
+                  spreadRadius: 2)
+            ],
+            color: Color(0xfffbb448)),
+        child: Text(
+          'Proceed to Login Screen',
+          style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  
+  Widget _title() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(children: [
+        TextSpan(
+          text: 'Budget',
+          style: TextStyle(color: Colors.white, fontSize: 30),
+        ),
+        TextSpan(
+          text: 'Go',
+          style: TextStyle(color: Color(0xffe46b10), fontSize: 30),
+        ),
+      ]),
+    );
+  }
+
+  Widget _text() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(children: [
+        TextSpan(
+          text: 'You are now logget out.\n\n\n',
+          style: TextStyle(color: Colors.white, fontSize: 20,fontWeight: FontWeight.bold),
+        ),
+        TextSpan(
+          text: 'Thank you for using BudgetGO mobile application. Hope you enjoyed your trip.',
+          style: TextStyle(color: Colors.white, fontSize: 17),
+        ),
+      ]),
+    );
+  }
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Container(
+      alignment: Alignment.center,
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 125,
+                ),
+                _title(),
+                SizedBox(
+                  height: 90,
+                ),
+                _text(),
+                SizedBox(
+                  height: 100,
+                ),
+                _submitButton(),
+                SizedBox(
+                  height: 5,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: SizedBox(),
+                ),
+              ],
+            ),
+          ),
+           Positioned(
+               top: -MediaQuery.of(context).size.height * .15,
+               right: -MediaQuery.of(context).size.width * .4,
+               child: BezierContainer())
+        ],
+      ),
+    )));
+  }
+}
