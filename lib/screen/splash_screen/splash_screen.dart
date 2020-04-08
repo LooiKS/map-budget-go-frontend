8c2286e5ef0a85c494e7148d100c2b895f995af4
CHEE KEAN CHIN <vinlyken@gmail.com>
2020-04-08 14:40:15 +0800

    Deleted unused function

--- lib/screen/splash_screen/splash_screen.dart
+++ lib/screen/splash_screen/splash_screen.dart
@@ -16,12 +16,12 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(Duration(seconds: 8), () {
-      // Navigator.of(context).pushReplacement(_createRoute());
       Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => LoginPage(toggleBrightness: widget.toggleBrightness),
-            ));
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                LoginPage(toggleBrightness: widget.toggleBrightness),
+          ));
     });
   }
 
@@ -35,50 +35,6 @@ class _SplashScreenState extends State<SplashScreen> {
         fit: BoxFit.contain,
         animation: "latest",
       ),
-    );
-  }
-
-  Route _createRoute() {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) =>
-          MyHomePage(toggleBrightness: widget.toggleBrightness),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        var begin = Offset(0.5, 1.0);
-        var end = Offset.zero;
-        var curve = Curves.ease;
-
-        var tween =
-            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-        return ScaleTransition(
-          scale: Tween<double>(begin: 0.0, end: 1.0).animate(
-            CurvedAnimation(
-              parent: animation,
-              curve: Interval(
-                0.00,
-                0.50,
-                curve: curve,
-              ),
-            ),
-          ),
-          child: ScaleTransition(
-            scale: Tween<double>(
-              begin: 1.5,
-              end: 1.0,
-            ).animate(
-              CurvedAnimation(
-                parent: animation,
-                curve: Interval(
-                  0.50,
-                  1.00,
-                  curve: curve,
-                ),
-              ),
-            ),
-            child: child,
-          ),
-        );
-      },
     );
   }
 }
