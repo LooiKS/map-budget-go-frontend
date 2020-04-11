d7a8d7c3aa9bf3d7d1e2ddfb896b9ffad7a10d54
Chee Kean Chin <vinlyken@gmail.com>
2020-04-11 16:03:57 +0800

    Refactoring the trip member list icon avatar

--- lib/screen/trips/trips_member_list.dart
+++ lib/screen/trips/trips_member_list.dart
@@ -4,8 +4,8 @@ class TripMemberList extends StatelessWidget {
   final Function(String) onTap;
   const TripMemberList({Key key, this.onTap}) : super(key: key);
 
-  final List<String> members = const ["John", "Maria"];
-  final List<String> image = const [
+  final List<String> dummyMembers = const ["John", "Maria"];
+  final List<String> dummyImage = const [
     "https://i.pinimg.com/736x/5a/0c/7b/5a0c7b76e2a8bcdbe571c5ba916f93fe.jpg",
     "https://cdn141.picsart.com/280218394017211.png?type=webp&to=min&r=640"
   ];
@@ -17,11 +17,9 @@ class TripMemberList extends StatelessWidget {
       child: ListView.separated(
         padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
         scrollDirection: Axis.horizontal,
-        itemCount: members.length + 1,
+        itemCount: dummyMembers.length + 1,
         itemBuilder: (context, index) {
-          return index != members.length
-              ? buildMemberAvatar(index)
-              : buildAddMemberButton();
+          return buildMemberAvatar(index);
         },
         separatorBuilder: (context, index) => SizedBox(
           width: 10.0,
@@ -30,38 +28,46 @@ class TripMemberList extends StatelessWidget {
     );
   }
 
-  GestureDetector buildAddMemberButton() {
-    return GestureDetector(
-      onTap: () {},
-      child: Column(children: <Widget>[
-        ClipOval(
-          child: Icon(
-            Icons.add_circle,
-            size: 40.0,
-            color: Colors.green,
-          ),
-        ),
-      ]),
-    );
-  }
+  // GestureDetector buildAddMemberButton() {
+  //   return GestureDetector(
+  //     onTap: () {},
+  //     child: Column(children: <Widget>[
+  //       ClipOval(
+  //         child: Icon(
+  //           Icons.add_circle,
+  //           size: 40.0,
+  //           color: Colors.green,
+  //         ),
+  //       ),
+  //     ]),
+  //   );
+  // }
 
   GestureDetector buildMemberAvatar(int index) {
     return GestureDetector(
       onTap: () {},
       child: Column(
         children: <Widget>[
-          ClipOval(
-            child: FadeInImage.assetNetwork(
-              placeholder: "assets/images/loading.gif",
-              image: image[index],
-              fit: BoxFit.contain,
-              width: 30.0,
-              height: 30.0,
-            ),
-          ),
+          index != 2
+              ? ClipOval(
+                  child: FadeInImage.assetNetwork(
+                    placeholder: "assets/images/loading.gif",
+                    image: dummyImage[index],
+                    fit: BoxFit.contain,
+                    width: 30.0,
+                    height: 30.0,
+                  ),
+                )
+              : ClipOval(
+                  child: Icon(
+                    Icons.add_circle,
+                    size: 30.0,
+                    color: Colors.green,
+                  ),
+                ),
           const SizedBox(height: 5.0),
           Text(
-            members[index],
+            index != 2 ? dummyMembers[index] : "Add",
             style: TextStyle(
               fontWeight: FontWeight.w500,
             ),
