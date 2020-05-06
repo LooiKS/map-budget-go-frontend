422c404e922cca7cd615a64205cb57ed1e7980e3
Chin Chee Kean <vinlyken@gmail.com>
2020-05-06 22:59:44 +0800

    Refactor the code

--- lib/screen/trips/trips_detail.dart
+++ lib/screen/trips/trips_detail.dart
@@ -4,7 +4,6 @@ import 'package:budgetgo/screen/trips/trips_edit.dart';
 import 'package:budgetgo/model/trip_expenses_class.dart';
 import 'package:budgetgo/screen/expenses/expenses_details.dart';
 import 'package:budgetgo/screen/expenses/expenses_screen.dart';
-import 'package:budgetgo/screen/trips/add_members.dart';
 import 'package:flutter/material.dart';
 import '../../widget/custom_shape.dart';
 import '../../main.dart';
@@ -19,7 +18,6 @@ class TripsDetail extends StatefulWidget {
 class _TripsDetailState extends State<TripsDetail> {
   List<Trips> dummyTrip = <Trips>[];
   List<TripExpenses> dummyExpenses = <TripExpenses>[];
-  // List<String> tripsDetailSetting = ['Trip Info', ';
 
   void _navigateEditTrips() async {
     Trips returnData = await Navigator.push(
@@ -35,6 +33,27 @@ class _TripsDetailState extends State<TripsDetail> {
     }
   }
 
+  Future<void> _tripEditError(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Trip Message'),
+          content: const Text(
+              'The trip is already over. You cannot edit this trip anymore.'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +61,8 @@ class _TripsDetailState extends State<TripsDetail> {
           ? Colors.transparent
           : Color.fromRGBO(244, 244, 244, 5),
       appBar: AppBar(
+        elevation: 0,
+        shape: CustomShapeBorder(),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () => Navigator.pop(context, widget.tripsData),
@@ -65,19 +86,17 @@ class _TripsDetailState extends State<TripsDetail> {
             },
           )
         ],
-        elevation: 0,
-        shape: CustomShapeBorder(),
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            buildCategoryTitle("Members"),
+            buildCategoryHeader("Members"),
             buildTripMemberList(
                 context, widget.tripsData.owner, widget.tripsData.members),
-            buildCategoryTitle("Schedule"),
+            buildCategoryHeader("Schedule"),
             buildSchedules(),
-            buildCategoryTitle("Expenses"),
+            buildCategoryHeader("Expenses"),
             buildExpenses(),
           ],
         ),
@@ -85,7 +104,7 @@ class _TripsDetailState extends State<TripsDetail> {
     );
   }
 
-  Padding buildCategoryTitle(String title) {
+  Padding buildCategoryHeader(String title) {
     return Padding(
       padding: const EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0),
       child: Row(
@@ -123,6 +142,58 @@ class _TripsDetailState extends State<TripsDetail> {
     );
   }
 
+  Container buildTripMemberList(
+      BuildContext context, User user, List<User> friendUser) {
+    List<User> memberList = [];
+    memberList.clear();
+    memberList.add(user);
+    memberList.addAll(friendUser);
+    return Container(
+      height: 70.0,
+      child: ListView.separated(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        scrollDirection: Axis.horizontal,
+        itemCount: friendUser.length + 1,
+        itemBuilder: (context, index) {
+          return buildMemberAvatar(index, memberList);
+        },
+        separatorBuilder: (context, index) => SizedBox(
+          width: 10.0,
+        ),
+      ),
+    );
+  }
+
+  GestureDetector buildMemberAvatar(int index, List<User> memberList) {
+    return GestureDetector(
+      child: Column(
+        children: <Widget>[
+          ClipOval(
+            child: FadeInImage.assetNetwork(
+              placeholder: "assets/images/loading.gif",
+              image: memberList[index].profilePic,
+              fit: BoxFit.contain,
+              width: 30.0,
+              height: 30.0,
+            ),
+          ),
+          const SizedBox(height: 5.0),
+          SizedBox(
+            width: 40.0,
+            child: Text(
+              memberList[index].firstName,
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+              ),
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Container buildSchedules() {
     return Container(
       height: 200.0,
@@ -157,25 +228,10 @@ class _TripsDetailState extends State<TripsDetail> {
               Icons.keyboard_arrow_right,
               size: 30.0,
             ),
-            title: Text(
-              // mockdata[0].schedules[index].activityTitle,
-              widget.tripsData.schedules[index].activityTitle,
-              style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: key.currentState.brightness == Brightness.dark
-                      ? Colors.white
-                      : Colors.black87),
-            ),
-            subtitle: Text(
-              // mockdata[0].schedules[index].activityDesc,
+            title:
+                buildItemTitle(widget.tripsData.schedules[index].activityTitle),
+            subtitle: buildItemSubTitle(
               widget.tripsData.schedules[index].activityDesc,
-              style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.w300,
-                  color: key.currentState.brightness == Brightness.dark
-                      ? Colors.white
-                      : Colors.black54),
             ),
             onTap: () {},
           ),
@@ -185,215 +241,100 @@ class _TripsDetailState extends State<TripsDetail> {
     );
   }
 
-  Container buildTripMemberList(
-      BuildContext context, User user, List<User> friendUser) {
-    List<User> memberList = [];
-    memberList.clear();
-    memberList.add(user);
-    memberList.addAll(friendUser);
-    return Container(
-      height: 70.0,
-      child: ListView.separated(
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-        scrollDirection: Axis.horizontal,
-        itemCount: friendUser.length + 1,
-        itemBuilder: (context, index) {
-          return buildMemberAvatar(index, memberList);
-        },
-        separatorBuilder: (context, index) => SizedBox(
-          width: 10.0,
-        ),
-      ),
-    );
-  }
-
-  GestureDetector buildMemberAvatar(int index, List<User> memberList) {
-    return GestureDetector(
-      onTap: () {},
-      child: Column(
-        children: <Widget>[
-          ClipOval(
-            child: FadeInImage.assetNetwork(
-              placeholder: "assets/images/loading.gif",
-              image: memberList[index].profilePic,
-              fit: BoxFit.contain,
-              width: 30.0,
-              height: 30.0,
-            ),
-          ),
-          const SizedBox(height: 5.0),
-          SizedBox(
-            width: 40.0,
-            child: Text(
-              memberList[index].firstName,
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-              ),
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   Container buildExpenses() {
     return Container(
       height: 200.0,
       child: ListView.builder(
-        shrinkWrap: true,
-        itemBuilder: (BuildContext ctxt, int index) => Card(
-          margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
-          child: ListTile(
-            selected: true,
-            leading: Container(
-                width: 48.0,
-                child: buildExpensesIcon(
-                    widget.tripsData.expenses[index].category)),
-            trailing: IconButton(
-              icon: Icon(
-                Icons.keyboard_arrow_right,
-                size: 30.0,
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ExpenseDetailsScreen(
-                            widget.tripsData.expenses[index])));
-              },
-            ),
-            title: Text(
-              widget.tripsData.expenses[index].title,
-              style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  color: key.currentState.brightness == Brightness.dark
-                      ? Colors.white
-                      : Colors.black87),
-            ),
-            subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Paid by ${widget.tripsData.expenses[index].payBy.firstName}",
-                    style: TextStyle(
-                        color: key.currentState.brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black54),
+          shrinkWrap: true,
+          itemCount: widget.tripsData.expenses.length,
+          itemBuilder: (BuildContext ctxt, int index) => Card(
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 16.0),
+                child: ListTile(
+                  selected: true,
+                  leading: Container(
+                      width: 48.0,
+                      child: buildExpensesIcon(
+                          widget.tripsData.expenses[index].category)),
+                  trailing: IconButton(
+                    icon: Icon(
+                      Icons.keyboard_arrow_right,
+                      size: 30.0,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ExpenseDetailsScreen(
+                                  widget.tripsData.expenses[index])));
+                    },
                   ),
-                  Text(
-                    widget.tripsData.expenses[index].createdDt.toString(),
-                    style: TextStyle(
-                        color: key.currentState.brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black54),
-                  ),
-                  Text(
-                    widget.tripsData.expenses[index].amount.toString(),
-                    style: TextStyle(
-                        color: key.currentState.brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black54),
-                  ),
-                ]),
-          ),
-        ),
-        itemCount: widget.tripsData.expenses.length,
-      ),
+                  title: buildItemTitle(widget.tripsData.expenses[index].title),
+                  subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        buildItemSubTitle(
+                            "Paid by ${widget.tripsData.expenses[index].payBy.firstName}"),
+                        buildItemSubTitle(widget
+                            .tripsData.expenses[index].createdDt
+                            .toString()),
+                        buildItemSubTitle(
+                            widget.tripsData.expenses[index].amount.toString()),
+                      ]),
+                ),
+              )),
     );
   }
 
-  Future _deleteConfirmationAlert(BuildContext context) {
-    return showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Delete Trip Confirmation'),
-          content: const Text(
-              'This will delete all the trip information from this application.'),
-          actions: <Widget>[
-            FlatButton(
-              child: const Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            FlatButton(
-              child: const Text('Confirm'),
-              onPressed: () {
-                setState(() {
-                  widget.tripsData.tripDetail = "cancel";
-                  Navigator.of(context).pop();
-                });
-                Navigator.pop(context, widget.tripsData);
-              },
-            )
-          ],
-        );
-      },
+  Text buildItemTitle(String title) {
+    return Text(
+      title,
+      style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          color: key.currentState.brightness == Brightness.dark
+              ? Colors.white
+              : Colors.black87),
     );
   }
 
-  Future<void> _tripEditError(BuildContext context) {
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Trip Message'),
-          content: const Text(
-              'The trip is already over. You cannot edit this trip anymore.'),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Ok'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+  Text buildItemSubTitle(String subTitle) {
+    return Text(
+      subTitle,
+      style: TextStyle(
+          color: key.currentState.brightness == Brightness.dark
+              ? Colors.white
+              : Colors.black54),
     );
   }
 
   Icon buildExpensesIcon(String title) {
     switch (title) {
       case "Hotel":
-        return Icon(
-          Icons.hotel,
-          size: 38.0,
-        );
+        return expensesLeadingIcon(Icons.hotel);
         break;
 
       case "Transport":
-        return Icon(
-          Icons.airport_shuttle,
-          size: 38.0,
-        );
+        return expensesLeadingIcon(Icons.airport_shuttle);
         break;
 
       case "Food & Beverage":
-        return Icon(
-          Icons.fastfood,
-          size: 38.0,
-        );
+        return expensesLeadingIcon(Icons.fastfood);
         break;
 
       case "Entertainment":
-        return Icon(
-          Icons.casino,
-          size: 38.0,
-        );
+        return expensesLeadingIcon(Icons.casino);
         break;
 
       default:
-        return Icon(
-          Icons.attach_money,
-          size: 38.0,
-        );
+        return expensesLeadingIcon(Icons.attach_money);
         break;
     }
+  }
+
+  Icon expensesLeadingIcon(IconData icon) {
+    return Icon(
+      icon,
+      size: 38.0,
+    );
   }
 }
